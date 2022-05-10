beea8db5deeb9362a1beb25191ef02bad73c17ff
fedyaka <68501592+fedyaka@users.noreply.github.com>
2022-05-10 22:49:04 +0300

    почти готовый вариант страницы blog
    так же добавлен подвал и тоже недоделан из за макета -_-

--- js/main.js
+++ js/main.js
@@ -4,7 +4,16 @@ $(function(){
     	$('.menu ul').slideToggle();
 	});
 
-	$('.slider_inner').slick();
+	// Слайдер
+	$('.slider_inner').slick({
+		infinite: true,
+		slidesToShow: 3,
+    	slidesToScroll: 1,
+		variableWidth: true,
+		lazyLoad: 'ondemand',
+		prevArrow: '<button type="button" class="slick-prev slick-btn"><span class="icon-caret-left icon"></span></button>',
+		nextArrow: '<button type="button" class="slick-next slick-btn"><span class="icon-caret-right icon"></span></button>'
+	});
 
 
 
